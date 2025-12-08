3b1ed587b5daa8ff8aba7ae1f16cc48077b6edc4
zampovaj <zampovaj.06@spst.eu>
2025-12-08 23:10:23 +0100

    final login fixes

--- Nyxon/Nyxon.Client/Services/LayoutService.cs
+++ Nyxon/Nyxon.Client/Services/LayoutService.cs
@@ -9,15 +9,27 @@ namespace Nyxon.Client.Services
     public class LayoutService
     {
         public bool IsLoggedIn { get; private set; } = false;
+        public bool IsVaultUnlocked { get; private set; } = false;
+
         public string? HeaderTitle { get; private set; } = "";
-        public string Username { get; private set;} = "";
+        public string Username { get; private set; } = "";
         public event Action? OnChange;
 
-        public void SetLoggedInState(bool isLoggedIn)
+        public void SetLoginState(bool isLoggedIn)
         {
             IsLoggedIn = isLoggedIn;
             if (!isLoggedIn) HeaderTitle = null;
             NotifyListeners();
+        }
+
+        // TODO: actually check
+        public void UnlockVault(string passphrase)
+        {
+            if (!string.IsNullOrWhiteSpace(passphrase))
+            {
+                IsVaultUnlocked = true;
+                NotifyListeners();
+            }
         }
         public void SetHeaderTitle(string? headerTitle)
         {
