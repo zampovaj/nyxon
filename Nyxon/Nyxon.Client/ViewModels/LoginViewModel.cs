8c5f4399173e60e74dbb9d1b7f1b739ecff79d03
zampovaj <zampovaj.06@spst.eu>
2025-12-18 12:16:10 +0100

    login view model validation

--- Nyxon/Nyxon.Client/ViewModels/LoginViewModel.cs
+++ Nyxon/Nyxon.Client/ViewModels/LoginViewModel.cs
@@ -2,7 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 using Nyxon.Client.Interfaces;
 
 namespace Nyxon.Client.ViewModels
@@ -15,11 +18,31 @@ namespace Nyxon.Client.ViewModels
         private readonly EncryptedUserVaultSessionService _userSessionService;
 
         [Required]
-        public string Username { get; set; } = "";
+        private string username = "";
+        public string Username
+        {
+            get => username;
+            set => username = value.Trim();
+        }
         [Required]
-        public string Password { get; set; } = "";
-        public string ConfirmPassword { get; set; } = "";
-        public string InviteCode { get; set; } = "";
+        private string password = "";
+        public string Password
+        {
+            get => password;
+            set => password = value.Trim();
+        }
+        private string confirmPassword = "";
+        public string ConfirmPassword
+        {
+            get => confirmPassword;
+            set => confirmPassword = value.Trim();
+        }
+        private string inviteCode = "";
+        public string InviteCode
+        {
+            get => inviteCode;
+            set => inviteCode = value.ToUpper().Trim();
+        }
 
         public bool IsRegistering { get; set; } = false;
         public string? ErrorMessage { get; set; } = "";
@@ -39,6 +62,66 @@ namespace Nyxon.Client.ViewModels
             _userSessionService = userSessionService;
         }
 
+        //validation
+        private bool IsNameValid(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name) ||
+            name.Length > 20 || name.Length < 5)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private bool IsPasswordValid(string password)
+        {
+            var pattern = @"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&_-])[A-Za-z\d@$!%*?&_-]{12,30}$";
+            return !string.IsNullOrWhiteSpace(password) && Regex.IsMatch(password, pattern);
+        }
+
+        private bool IsInviteCodeValid(string code)
+        {
+            if (code.Any(c => !char.IsLetterOrDigit(c)))
+                return false;
+            if (code.Length != 12)
+                return false;
+
+            return true;
+        }
+
+        private bool IsConfirmValid(string confirmPassword) => Password == confirmPassword;
+
+        private bool Validate()
+        {
+            if (!IsNameValid(Username))
+            {
+                ErrorMessage = "Username invalid. Username must be between 5 and 20 characters long.";
+                return false;
+            }
+            if (!IsPasswordValid(Password))
+            {
+                ErrorMessage = "Password invalid. Password must be between 12 and 30 characters long. Password must contain at least one number, uppercase letter, lowercase letter and special character.";
+                return false;
+            }
+
+            if (IsRegistering)
+            {
+                if (!IsInviteCodeValid(InviteCode))
+                {
+                    ErrorMessage = "Invite code invalid. Invite cide must be 12 characters long and contain only letters and numbers";
+                    return false;
+                }
+                if (!IsConfirmValid(ConfirmPassword))
+                {
+                    ErrorMessage = "Passwords don't match.";
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        //methods
+
         public void ToggleMode()
         {
             IsRegistering = !IsRegistering;
@@ -57,15 +140,15 @@ namespace Nyxon.Client.ViewModels
 
             bool success = false;
 
+            if (!Validate())
+            {
+                IsBusy = false;
+                Notify();
+                return;
+            }
+
             if (IsRegistering)
             {
-                if (Password != ConfirmPassword)
-                {
-                    ErrorMessage = "Passwords dont match.";
-                    IsBusy = false;
-                    Notify();
-                    return;
-                }
                 success = await _authService.RegisterAsync(Username, Password, InviteCode);
             }
             else
@@ -88,7 +171,6 @@ namespace Nyxon.Client.ViewModels
                 ErrorMessage = IsRegistering ? "Registration failed." : "Invalid credentials or login failed.";
             }
             Notify();
-
         }
 
         public void Notify() => StateChanged?.Invoke();
