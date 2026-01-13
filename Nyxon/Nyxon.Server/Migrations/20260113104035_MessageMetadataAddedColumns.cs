c631eba642ed3952e4977b8a0198cdf58477bfec
zampovaj <zampovaj.06@spst.eu>
2026-01-13 11:41:02 +0100

    messages: migrations: added columns to message metadata

---
+++ Nyxon/Nyxon.Server/Migrations/20260113104035_MessageMetadataAddedColumns.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nyxon.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class MessageMetadataAddedColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<byte[]>(
+                name: "EncryptedPayload",
+                table: "MessageMetadata",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SequenceNumber",
+                table: "MessageMetadata",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EncryptedPayload",
+                table: "MessageMetadata");
+
+            migrationBuilder.DropColumn(
+                name: "SequenceNumber",
+                table: "MessageMetadata");
+        }
+    }
+}
