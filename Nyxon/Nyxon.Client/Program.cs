37c516ef8466f2b57751dd3eaaedab6c1ec0d00b
zampovaj <zampovaj.06@spst.eu>
2026-03-31 01:26:05 +0200

    logging level

--- Nyxon/Nyxon.Client/Program.cs
+++ Nyxon/Nyxon.Client/Program.cs
@@ -7,6 +7,9 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// logging
+builder.Logging.SetMinimumLevel(LogLevel.Warning); 
+
 // register everything
 builder.Services.AddClientServices(builder.HostEnvironment);
 builder.Services.AddCoreServices();
